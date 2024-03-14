1c50a9eb2323762a68268d07e929300000891114
Daniella Calle <daniella.calle@gmail.com>
2024-03-14 00:02:12 -0400

    lint again

--- frontend/src/components/Login/CreateAccount.tsx
+++ frontend/src/components/Login/CreateAccount.tsx
@@ -12,36 +12,35 @@ export default function CreateAccount() {
   function createAcc() {
     setIsCreating(true);
     createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // after creating the account, the user is automatically logged in,
-      // so we can redirect them to somewhere useful once we have that
-      const user = userCredential.user;
-      setLoggedInUser(user);
-    })
-    .catch((error: Error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorCode, errorMessage)
-      
-    });
+      .then(userCredential => {
+        // after creating the account, the user is automatically logged in,
+        // so we can redirect them to somewhere useful once we have that
+        const user = userCredential.user;
+        setLoggedInUser(user);
+      })
+      .catch((error: Error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
 
-    setIsCreating(false)
+    setIsCreating(false);
   }
 
   return (
-      <>
-      {
-        (!loggedInUser && (
+    <>
+      {(!loggedInUser && (
         <>
-        <Heading>Create Account</Heading>
-        <FormControl>
+          <Heading>Create Account</Heading>
+          <FormControl>
             <FormLabel htmlFor='email'>Email</FormLabel>
             <Input
               autoFocus
               name='email'
               placeholder='Your email'
               value={email}
-              onChange={event => setEmail(event.target.value)} />
+              onChange={event => setEmail(event.target.value)}
+            />
 
             <FormLabel htmlFor='password'>Password</FormLabel>
             <Input
@@ -49,21 +48,22 @@ export default function CreateAccount() {
               name='password'
               placeholder='Password'
               value={password}
-              onChange={event => setPassword(event.target.value)} />
+              onChange={event => setPassword(event.target.value)}
+            />
           </FormControl>
           <Button
             data-testid='joinTownByIDButton'
             onClick={() => createAcc()}
             isLoading={isCreating}
             disabled={isCreating}>
-              Connect
-            </Button>
-            </>)
-            || <>
-            <Text>Logged in as {loggedInUser?.displayName}</Text>
-            </>
-      )
-      }
-      </>
+            Connect
+          </Button>
+        </>
+      )) || (
+        <>
+          <Text>Logged in as {loggedInUser?.displayName}</Text>
+        </>
+      )}
+    </>
   );
 }
