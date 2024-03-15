08de2bf31336d952e2f29ad55481d2369bef26f4
zatchet <zach@rippas.org>
2024-03-14 23:03:11 -0400

    Update userService.ts

--- userService/userService.ts
+++ userService/userService.ts
@@ -1,12 +1,12 @@
 // imports the firebase config file at project root
-import { collection, Firestore } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 // eslint-disable-next-line import/no-relative-packages
 import { db } from '../firebaseconfig';
 
 export default class UserService {
   public static async getGameHistory(userId: string): Promise<any> {
     // retrieves a user's game history from the firestore db
-    const querySnapshot = collection(db, 'test_collection');
-    console.log(querySnapshot);
+    const querySnapshot = await getDocs(collection(db, 'GameHistory'));
+    return querySnapshot.docs.filter(doc => doc.id === userId)?.[0]?.data();
   }
 }
