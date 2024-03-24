adcd1b50fde4a10fe3b550ae18a877380c12f9ce
jamesgu210 <130703110+jamesgu210@users.noreply.github.com>
2024-03-24 17:10:19 -0400

    Quick update to fix passing in app instance to auth

--- frontend/src/components/Login/SignInInput.tsx
+++ frontend/src/components/Login/SignInInput.tsx
@@ -1,9 +1,10 @@
 import { Button, Heading, Input } from '@chakra-ui/react';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react';
+import { firebaseApp } from '../../../../firebaseconfig';
 
 export default function SignInInput() {
-  const auth = getAuth();
+  const auth = getAuth(firebaseApp);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isSignedIn, setIsSignedIn] = useState(false);
