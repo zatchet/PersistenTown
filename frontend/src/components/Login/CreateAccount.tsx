3a39d13743cccb91ab3261a03a59b672b506c7dc
Daniella Calle <daniella.calle@gmail.com>
2024-04-08 13:53:22 -0400

    require all fields, parse error msgs

--- frontend/src/components/Login/CreateAccount.tsx
+++ frontend/src/components/Login/CreateAccount.tsx
@@ -1,12 +1,21 @@
-import { Heading, Button, FormControl, FormLabel, Input, Box, useToast } from '@chakra-ui/react';
+import {
+  Heading,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
+  useToast,
+  FormErrorMessage,
+} from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { createUserWithEmailAndPassword, User, updateProfile } from 'firebase/auth';
 import { auth } from '../../classes/users/firebaseconfig';
 
 export default function CreateAccount() {
-  const [displayName, setDisplayName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState<string | undefined>(undefined);
+  const [email, setEmail] = useState<string | undefined>(undefined);
+  const [password, setPassword] = useState<string | undefined>(undefined);
   const [isCreating, setIsCreating] = useState(false);
   const [loggedInUser, setLoggedInUser] = useState<User | undefined>(undefined);
   const toast = useToast();
@@ -14,6 +23,20 @@ export default function CreateAccount() {
   function extractErrorMsg(error: Error) {
     const firebaseLength = 'Firebase: '.length;
     return error.message.substring(firebaseLength, error.message.length - 1);
+  }
+
+  function errorToStr(error: Error) {
+    if (error.message.startsWith('Firebase: Error (auth/email-already-in-use)')) {
+      return 'Email address is already in use. Please use a different email address.';
+    } else if (
+      error.message.startsWith(
+        'Firebase: Password should be at least 6 characters (auth/weak-password)',
+      )
+    ) {
+      return 'Password must be at least 6 characters.';
+    } else {
+      return extractErrorMsg(error);
+    }
   }
 
   function resetForm() {
@@ -25,6 +48,28 @@ export default function CreateAccount() {
 
   function createAcc() {
     setIsCreating(true);
+    if (
+      displayName === undefined ||
+      displayName === '' ||
+      email === undefined ||
+      email === '' ||
+      password === undefined ||
+      password === ''
+    ) {
+      console.log('displayname: ', displayName);
+      console.log('email: ', email);
+      console.log('password: ', password);
+      resetForm();
+      toast({
+        title: 'Error creating account',
+        description: 'All fields are required.',
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      });
+      setIsCreating(false);
+      return;
+    }
     createUserWithEmailAndPassword(auth, email, password)
       .then(userCredential => {
         const user = userCredential.user;
@@ -39,7 +84,7 @@ export default function CreateAccount() {
         resetForm();
         toast({
           title: 'Error creating account',
-          description: extractErrorMsg(error),
+          description: errorToStr(error),
           status: 'error',
           duration: 9000,
           isClosable: true,
@@ -47,18 +92,21 @@ export default function CreateAccount() {
       });
   }
 
+  const formFilled = displayName !== '' && email !== '' && password != '';
+
   return (
     <Box mb='2' p='4' borderWidth='1px' borderRadius='lg'>
       {!loggedInUser && (
         <>
           <Heading>Create Account</Heading>
-          <FormControl>
+          <FormControl isRequired isInvalid={!formFilled}>
             <FormLabel htmlFor='displayName'>Display Name</FormLabel>
             <Input
               autoFocus
               name='displayName'
               placeholder='Display name'
               value={displayName}
+              isRequired={true}
               onChange={event => setDisplayName(event.target.value)}
             />
             <FormLabel htmlFor='email'>Email</FormLabel>
@@ -67,6 +115,8 @@ export default function CreateAccount() {
               name='email'
               placeholder='Your email'
               value={email}
+              type='email'
+              isRequired={true}
               onChange={event => setEmail(event.target.value)}
             />
             <FormLabel htmlFor='password'>Password</FormLabel>
@@ -76,15 +126,17 @@ export default function CreateAccount() {
               placeholder='Password'
               value={password}
               type='password'
+              isRequired={true}
               onChange={event => setPassword(event.target.value)}
             />
+            {!formFilled && <FormErrorMessage>All fields are required.</FormErrorMessage>}
           </FormControl>
           <Button
             data-testid='joinTownByIDButton'
             onClick={() => createAcc()}
             isLoading={isCreating}
             disabled={isCreating}>
-            Connect
+            Create Account
           </Button>
         </>
       )}
