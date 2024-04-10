27193829f4d17c975276f0dcbe2db5f77aa9f4c0
Daniella Calle <daniella.calle@gmail.com>
2024-04-09 21:49:52 -0400

    remove extra read

--- frontend/src/classes/users/userNameService.ts
+++ frontend/src/classes/users/userNameService.ts
@@ -11,9 +11,6 @@ export async function userNameExists(username: string): Promise<boolean> {
 
 // adds a username to the firestore db
 export async function addUsernameIfNotPresent(username: string, uid: string): Promise<void> {
-  if (await userNameExists(username)) {
-    throw new Error('non-unique username');
-  }
   const docRef = doc(db, COLLECTION, username);
   await setDoc(docRef, {
     uid: uid,
